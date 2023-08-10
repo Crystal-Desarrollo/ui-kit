123d317ba67064895370cd02524b14ed34ed565d
Agustin Zamar <agustinzamar33@gmail.com>
2023-08-10 20:36:34 -0300

    test

--- src/routes/PrivateRoute/PrivateRoute.jsx
+++ src/routes/PrivateRoute/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import {Navigate, useLocation} from 'react-router-dom';
-import {useAuth} from '@/hooks/useAuth/useAuth.js';
-import {hasPermissions} from '@/utils/Auth/Auth.js';
+import {useAuth} from '@/hooks';
+import {hasPermissions} from '@/utils';
 import PropTypes from 'prop-types';
 
 export const PrivateRoute = ({has, element}) => {
