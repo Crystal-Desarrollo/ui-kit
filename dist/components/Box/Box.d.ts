b0ecea12c56c19430d4e840cf7fc56c822ac795f
Agustin Zamar <agustinzamar33@gmail.com>
2023-08-11 16:57:43 -0300

    v0.6.1

--- dist/components/Box/Box.d.ts
+++ dist/components/Box/Box.d.ts
@@ -1,27 +1,21 @@
 export default Box;
-declare function Box({ children, style, sx, component, onSubmit, mb, mt, isLoading, ...rest }: {
+declare function Box({ children, style, sx, component, noSpacing, isLoading, ...rest }: {
     [x: string]: any;
     children: any;
     style: any;
     sx: any;
     component: any;
-    onSubmit: any;
-    mb: any;
-    mt: any;
+    noSpacing?: boolean | undefined;
     isLoading?: boolean | undefined;
 }): import("react/jsx-runtime").JSX.Element;
 declare namespace Box {
     namespace propTypes {
-        const children: PropTypes.Requireable<PropTypes.ReactNodeLike>;
+        const children: PropTypes.Validator<NonNullable<PropTypes.ReactNodeLike>>;
         const style: PropTypes.Requireable<object>;
         const sx: PropTypes.Requireable<object>;
         const component: PropTypes.Requireable<string>;
-        const onSubmit: PropTypes.Requireable<(...args: any[]) => any>;
+        const noSpacing: PropTypes.Requireable<boolean>;
         const isLoading: PropTypes.Requireable<boolean>;
-        const marginBottom: PropTypes.Requireable<string>;
-        const marginTop: PropTypes.Requireable<string>;
-        const mb: PropTypes.Requireable<number>;
-        const mt: PropTypes.Requireable<number>;
     }
 }
 import PropTypes from "prop-types";
