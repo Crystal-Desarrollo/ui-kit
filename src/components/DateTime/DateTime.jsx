c30b9d583f56e96d36e9089da0ab327557046685
Agustin Zamar <agustinzamar33@gmail.com>
2023-09-16 03:37:00 -0300

    improve DateTime.jsx

--- src/components/DateTime/DateTime.jsx
+++ src/components/DateTime/DateTime.jsx
@@ -1,30 +1,27 @@
 import PropTypes from 'prop-types';
-import { format, formatRelative, isDate, isValid, parse } from 'date-fns';
+import { format, formatRelative, isDate, isValid, parse, parseISO } from 'date-fns';
 import { es } from 'date-fns/locale';
 
 const DateTime = ({ isoDateTimeString, relative = false, showTime = true }) => {
-  const dateFormat =
-    isoDateTimeString.length > 10
-      ? "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS"
-      : 'yyyy-MM-dd';
+  let date = isDate(isoDateTimeString) ? isoDateTimeString : parseISO(isoDateTimeString);
 
-  const date = isDate(isoDateTimeString)
-    ? isoDateTimeString
-    : parse(isoDateTimeString, dateFormat, new Date());
+  if (!isValid(date)) {
+    const dateFormat = isoDateTimeString.length >= 10 ? "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS" : 'yyyy-MM-dd';
+    date = parse(isoDateTimeString, dateFormat, new Date());
+  }
 
   if (!isValid(date)) return <span>Fecha inválida</span>;
 
   const formatString = showTime ? 'dd/MM/yyyy HH:mm' : 'dd/MM/yyyy';
-
-  if (!relative) return <span>{format(date, formatString)}</span>;
-
-  return (
-    <span>
-      {formatRelative(date, new Date(), {
+  const formattedDate = relative
+    ? formatRelative(date, new Date(), {
         locale: es,
-      })}
-    </span>
-  );
+      })
+    : format(date, formatString, {
+        locale: es,
+      });
+
+  return <span>{formattedDate}</span>;
 };
 
 DateTime.propTypes = {
